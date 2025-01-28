0e1f56e972634cf4b2659255dd0421a4bf76ec69
Paola Orozco <paoliorozco22@gmail.com>
2025-01-28 18:54:31 -0500

    Add new message

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById('changeTextButton').addEventListener('click', function() {
     document.getElementById('textToChange').textContent = '¡El texto ha cambiado!';
 });
+//no tiene sentido
+
